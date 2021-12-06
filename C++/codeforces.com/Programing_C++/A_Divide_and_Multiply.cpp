5c3985f8c84f9fe1fba3a5aefeab5f29d7ed700e
Sohan <sohancms@gmail.com>
2021-12-07 01:41:43 +0600

    new_added

---
+++ C++/codeforces.com/Programing_C++/A_Divide_and_Multiply.cpp
@@ -0,0 +1,85 @@
+//--------------------------------*বিসমিল্লাহির_রহমানির_রহীম*----------------------------------//
+/*********************************************************************************************
+||                 IN THE NAME OF ALLAH, MOST GRACIOUS AND MOST MERCIFUL                    ||
+||                                                                                          ||
+||    Author     : Md.Sohan Mia                                                             ||
+||    Email      : sohancms@gmail.com                                                       ||
+||    University : Bangamata Sheikh Fojilatunnesa Mujib Science and Technology University   ||
+||                                                                                          ||
+||                 Department of Computer Science & Engineering                             ||
+||                                                                                          ||
+*********************************************************************************************/
+
+///*****************************************************************************************
+///***************START************************START************************START***********
+///*****************************************************************************************
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <algorithm>
+#include <climits>
+//====================================> define <=====================================
+#define PI acos(-1.0)
+#define f first
+#define s second
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define pof pop_front
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define sort(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+#define ima INT_MAX
+#define imi INT_MIN
+#define rtn return
+
+using namespace std;
+
+//====================================> typedef <====================================
+typedef long long ll;
+typedef long double ld;
+typedef double db;
+typedef string str;
+//=====================================> const <=====================================
+const ll mod = 1000000007;
+
+void solve()
+{
+    ll n,count=0;
+    cin >> n;
+    vector<int> vec(n, 0);
+    for (size_t i = 0; i < n; i++)
+    {
+        cin >> vec[i];
+    }
+    for (size_t i = 0; i < n; i++)
+    {
+        while (vec[i]%2==0)
+        {
+            vec[i]/=2;
+            count++;
+        }
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll test;
+    cin >> test;
+    while (test--)
+    {
+        solve();
+    }
+    return 0;
+}
+//**************************Alhamdulillah**************************//
+//**************************Alhamdulillah**************************//
